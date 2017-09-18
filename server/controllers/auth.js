a332bad40199328dc32d973d9c9eeee607cbbd1f
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-18 14:40:14 -0400

    Added JWT

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function createUserToken(user){
+	let timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next){
 	
@@ -31,7 +38,7 @@ exports.signup = function(req, res, next){
 				return next(err);
 			}
 			//Respond to request indicating the user was created
-			res.json({ success: true });
+			res.json({ token: createUserToken(user) });
 		});
 	});
 }
