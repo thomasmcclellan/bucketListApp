5edf6e6e9eab192ef9958f6114882d2d4ab39b4c
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-20 10:35:45 -0400

    Added new list item

---
+++ client/src/list/new-list-item.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
+
+class ListItem extends Component{
+	handleFormSubmit(formProps){
+		//call action creator to sign up the user
+		console.log(formProps);
+	}
+	render(){
+		const { fields: { title, topic, url, content }, handleSubmit } = this.props;
+		return (
+			<form onSubmit={ handleSubmit(this.handleFormSubmit.bind(this)) }>
+				<h3>CREATE a NEW Post!</h3>
+				<fieldset className='form-group'>
+					<label>TITLE</label>
+					<input type='text' className='form-control' { ...title } />
+				</fieldset>
+				<fieldset className='form-group'>
+					<label>CATEGORY</label>
+					<input type='text' className='form-control' { ...topic } />
+				</fieldset>
+				<fieldset className='form-group'>
+					<label>URL</label>
+					<input type='text' className='form-control' { ...url } />
+				</fieldset>
+				<fieldset className='form-group'>
+					<label>CONTENT</label>
+					<input type='text' rows='8' className='form-control text' { ...content } />
+				</fieldset>
+
+				<button type='submit' className='btn btn-primary'>SUBMIT</button>
+				<Link to='/' className='btn btn-danger'>CANCEL</Link>
+			</form>
+		);
+	}
+}
+export default reduxForm({
+	form: 'PostsNewForm',
+	fields: ['title', 'topic', 'url', 'content']
+})(ListItem);
